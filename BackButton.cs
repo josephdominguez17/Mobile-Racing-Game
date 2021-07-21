bdf0eff8a8847538dd35e977da4ea474936ab6bf
josephdominguez17 <62165156+josephdominguez17@users.noreply.github.com>
2021-07-21 18:06:15 +1000

    Add files via upload

---
+++ BackButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityStandardAssets.Vehicles.Car;
+
+public class BackButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+
+
+    public static BackButton instance = null;
+
+    CarUserControl car;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+    }
+
+    public void SetPlayer(GameObject player)
+    {
+        car = player.GetComponent<CarUserControl>();
+    }
+
+    public void OnPointerDown(PointerEventData evenData)
+    {
+        car.Backward();
+    }
+
+    public void OnPointerUp(PointerEventData evenData)
+    {
+        car.PointerUP();
+    }
+
+
+}
---
+++ ForwardButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityStandardAssets.Vehicles.Car;
+
+//IPointerDownHandler, IPointerUpHandler
+public class ForwardButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public static ForwardButton instance = null;
+
+    CarUserControl car;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+    }
+
+    public void SetPlayer(GameObject player)
+    {
+        car = player.GetComponent<CarUserControl>();
+    }
+
+    public void OnPointerDown(PointerEventData evenData)
+    {
+        car.Forward();
+    }
+
+    public void OnPointerUp(PointerEventData evenData)
+    {
+        car.PointerUP();
+    }
+
+
+
+
+}
